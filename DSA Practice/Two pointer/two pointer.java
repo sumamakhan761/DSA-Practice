2eb870a693b367389d5ad86ec33e4d498d214b3b
sumamakhan7611 <khansumama87111@gmail.com>
2025-01-30 16:32:12 +0530

    DSA Practice

---
+++ DSA Practice/Two pointer/two pointer.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/apply-operations-to-an-array/description/
+
+// https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/description/
+
+// https://leetcode.com/problems/bag-of-tokens/description/?envType=problem-list-v2&envId=two-pointers
+
+// https://leetcode.com/problems/circular-array-loop/description/?envType=problem-list-v2&envId=two-pointers
+
+// https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/description/?envType=problem-list-v2&envId=two-pointers
+
+// https://leetcode.com/problems/maximum-matching-of-players-with-trainers/description/?envType=problem-list-v2&envId=two-pointers
+
+// https://leetcode.com/problems/find-k-th-smallest-pair-distance/description/?envType=problem-list-v2&envId=two-pointers
+
+// https://leetcode.com/problems/last-substring-in-lexicographical-order/description/?envType=problem-list-v2&envId=two-pointers
