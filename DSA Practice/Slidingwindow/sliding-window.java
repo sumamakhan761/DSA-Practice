36da56450e296ea4361c0901b3f94a2471442d83
sumamakhan7611 <khansumama87111@gmail.com>
2025-02-02 14:09:13 +0530

    sliding window

--- DSA Practice/Slidingwindow/sliding-window.java
+++ DSA Practice/Slidingwindow/sliding-window.java
@@ -1,9 +1,3 @@
-// https://leetcode.com/problems/minimum-positive-sum-subarray/description/?envType=problem-list-v2&envId=sliding-window
-
-// https://leetcode.com/problems/maximum-subarray-with-equal-products/description/?envType=problem-list-v2&envId=sliding-window
-
-// https://leetcode.com/problems/minimum-size-subarray-sum/description/?envType=problem-list-v2&envId=sliding-window
-
 // https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/description/?envType=problem-list-v2&envId=sliding-window
 
 // https://leetcode.com/problems/longest-even-odd-subarray-with-threshold/description/?envType=problem-list-v2&envId=sliding-window
@@ -16,3 +10,12 @@
 
 // https://leetcode.com/problems/binary-subarrays-with-sum/solutions/186683/c-java-python-sliding-window-o-1-space/
 
+// https://leetcode.com/problems/max-consecutive-ones-iii/discuss/247564/javacpython-sliding-window/379427?page=3
+
+// https://leetcode.com/problems/k-radius-subarray-averages/solutions/?envType=problem-list-v2&envId=sliding-window
+
+// https://leetcode.com/problems/grumpy-bookstore-owner/description/?envType=problem-list-v2&envId=sliding-window
+
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description/?envType=problem-list-v2&envId=sliding-window
+
+// https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/description/?envType=problem-list-v2&envId=sliding-window
