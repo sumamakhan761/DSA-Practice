f78d39d254707fc295e2ed2af5ba631e560f3ccc
sumamakhan7611 <khansumama87111@gmail.com>
2025-02-05 15:11:42 +0530

    bit-minupilation

---
+++ DSA Practice/bit-manipulation/bit.java
@@ -0,0 +1,13 @@
+
+// https://leetcode.com/problems/single-number/description/
+
+// https://leetcode.com/problems/missing-number/description/
+
+// https://leetcode.com/problems/count-paths-with-the-given-xor-value/description/
+
+
+// https://leetcode.com/problems/binary-gap/?envType=problem-list-v2&envId=bit-manipulation
+
+// https://leetcode.com/problems/divide-two-integers/description/?envType=problem-list-v2&envId=bit-manipulation
+
+// https://leetcode.com/problems/single-number-ii/description/?envType=problem-list-v2&envId=bit-manipulation
