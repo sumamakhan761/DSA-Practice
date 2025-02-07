b351a8d95b37749cd4ae23686915d559f2667b6b
sumamakhan7611 <khansumama87111@gmail.com>
2025-02-07 13:53:01 +0530

    recursion

---
+++ DSA Practice/recursion/recursion.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/powx-n/description/?envType=problem-list-v2&envId=recursion
+
+// https://leetcode.com/problems/power-of-two/description/?envType=problem-list-v2&envId=recursion
+
+// https://leetcode.com/problems/power-of-three/description/?envType=problem-list-v2&envId=recursion
+
+// https://leetcode.com/problems/count-good-numbers/description/?envType=problem-list-v2&envId=recursion
+
+// https://leetcode.com/problems/permutation-sequence/description/?envType=problem-list-v2&envId=recursion
+
+// https://leetcode.com/problems/integer-to-english-words/description/?envType=problem-list-v2&envId=recursion
+
+// https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/description/?envType=problem-list-v2&envId=recursion
