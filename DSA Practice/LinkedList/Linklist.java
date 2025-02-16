994435bfac2f6a024786b64d0ffed76b47b16de8
sumamakhan761 <164807432+sumamakhan761@users.noreply.github.com>
2025-02-16 12:08:53 +0530

    Update Linklist.java

--- DSA Practice/LinkedList/Linklist.java
+++ DSA Practice/LinkedList/Linklist.java
@@ -19,3 +19,21 @@ https://leetcode.com/problems/linked-list-cycle-ii/description/?envType=problem-
 https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/?envType=problem-list-v2&envId=linked-list
 
 https://leetcode.com/problems/lru-cache/description/?envType=problem-list-v2&envId=linked-list
+
+https://leetcode.com/problems/add-two-numbers/description/?envType=problem-list-v2&envId=linked-list
+
+https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/?envType=problem-list-v2&envId=linked-list
+
+https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/?envType=problem-list-v2&envId=linked-list
+
+https://leetcode.com/problems/spiral-matrix-iv/description/?envType=problem-list-v2&envId=linked-list
+
+https://leetcode.com/problems/split-linked-list-in-parts/description/?envType=problem-list-v2&envId=linked-list
+
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/?envType=problem-list-v2&envId=linked-list
+
+https://leetcode.com/problems/swap-nodes-in-pairs/description/?envType=problem-list-v2&envId=linked-list
+
+https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/description/?envType=problem-list-v2&envId=linked-list
+
+https://leetcode.com/problems/copy-list-with-random-pointer/description/?envType=problem-list-v2&envId=linked-list
