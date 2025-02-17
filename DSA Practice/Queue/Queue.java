8e705fcbe3d3b2c16db812d094381b3b84d6044c
sumamakhan761 <164807432+sumamakhan761@users.noreply.github.com>
2025-02-17 13:30:40 +0530

    Update Queue.java

--- DSA Practice/Queue/Queue.java
+++ DSA Practice/Queue/Queue.java
@@ -1,1 +1,19 @@
+https://leetcode.com/problems/implement-queue-using-stacks/description/?envType=problem-list-v2&envId=queue
 
+https://leetcode.com/problems/implement-stack-using-queues/description/?envType=problem-list-v2&envId=queue
+
+https://leetcode.com/problems/first-unique-character-in-a-string/description/?envType=problem-list-v2&envId=queue
+
+https://leetcode.com/problems/sliding-window-maximum/description/?envType=problem-list-v2&envId=queue
+
+https://leetcode.com/problems/number-of-recent-calls/description/?envType=problem-list-v2&envId=queue
+
+https://leetcode.com/problems/reveal-cards-in-increasing-order/description/?envType=problem-list-v2&envId=queue
+
+https://leetcode.com/problems/find-the-winner-of-the-circular-game/description/?envType=problem-list-v2&envId=queue
+
+https://leetcode.com/problems/design-circular-deque/description/?envType=problem-list-v2&envId=queue
+
+https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips/description/?envType=problem-list-v2&envId=queue
+
+https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/?envType=problem-list-v2&envId=queue
