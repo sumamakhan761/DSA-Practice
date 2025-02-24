bd4fddb0475a10cebcf3bb652a30bb06359fa18b
sumamakhan761 <164807432+sumamakhan761@users.noreply.github.com>
2025-02-24 15:22:33 +0530

    Update hashmap.java

--- DSA Practice/HashMap/hashmap.java
+++ DSA Practice/HashMap/hashmap.java
@@ -29,3 +29,23 @@ https://leetcode.com/problems/sort-characters-by-frequency/description/
 https://leetcode.com/problems/subarray-sum-equals-k/description/
 
 https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+https://leetcode.com/problems/4sum-ii/description/?envType=problem-list-v2&envId=hash-table
+
+https://leetcode.com/problems/longest-repeating-character-replacement/description/?envType=problem-list-v2&envId=hash-table
+
+https://leetcode.com/problems/find-all-anagrams-in-a-string/description/?envType=problem-list-v2&envId=hash-table
+
+https://leetcode.com/problems/find-all-duplicates-in-an-array/description/?envType=problem-list-v2&envId=hash-table
+
+https://leetcode.com/problems/bulls-and-cows/description/?envType=problem-list-v2&envId=hash-table
+
+https://leetcode.com/problems/count-number-of-bad-pairs/description/?envType=problem-list-v2&envId=hash-table
+
+https://leetcode.com/problems/equal-row-and-column-pairs/description/?envType=problem-list-v2&envId=hash-table
+
+https://leetcode.com/problems/unique-length-3-palindromic-subsequences/description/?envType=problem-list-v2&envId=hash-table
+
+https://leetcode.com/problems/optimal-partition-of-string/description/?envType=problem-list-v2&envId=hash-table
+
+https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/description/?envType=problem-list-v2&envId=hash-table
