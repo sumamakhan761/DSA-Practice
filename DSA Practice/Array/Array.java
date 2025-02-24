8d7b62d4b85e931c92ef56566ed3ba17be9c4eea
sumamakhan761 <164807432+sumamakhan761@users.noreply.github.com>
2025-02-24 15:30:04 +0530

    Create Array.java

---
+++ DSA Practice/Array/Array.java
@@ -0,0 +1,8 @@
+https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+https://leetcode.com/problems/move-zeroes/description/
+
+https://leetcode.com/problems/max-consecutive-ones/description/
+
