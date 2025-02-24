a4faba896a6ebecc95ebfa54d028a5b548ba0825
sumamakhan761 <164807432+sumamakhan761@users.noreply.github.com>
2025-02-24 15:32:45 +0530

    Update and rename Array.java to Array and Metrix.java

--- DSA Practice/Array/Array.java
+++
@@ -1,8 +0,0 @@
-https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
-
-https://leetcode.com/problems/remove-duplicates-from-sorted-array/
-
-https://leetcode.com/problems/move-zeroes/description/
-
-https://leetcode.com/problems/max-consecutive-ones/description/
-
