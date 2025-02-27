a649878ddb24f6607f1eaa830f5df271ddea3c63
sumamakhan761 <164807432+sumamakhan761@users.noreply.github.com>
2025-02-27 17:36:40 +0530

    Update Binary_Search.java

--- DSA Practice/Binary Search/Binary_Search.java
+++ DSA Practice/Binary Search/Binary_Search.java
@@ -7,3 +7,13 @@ https://leetcode.com/problems/missing-number/description/?envType=problem-list-v
 https://leetcode.com/problems/binary-search/description/?envType=problem-list-v2&envId=binary-search
 
 https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/description/?envType=problem-list-v2&envId=binary-search
+
+https://leetcode.com/problems/search-insert-position/description/?envType=problem-list-v2&envId=binary-search
+
+https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/?envType=problem-list-v2&envId=binary-search
+
+https://leetcode.com/problems/missing-number/description/?envType=problem-list-v2&envId=binary-search
+
+https://leetcode.com/problems/binary-search/description/?envType=problem-list-v2&envId=binary-search
+
+https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/description/?envType=problem-list-v2&envId=binary-search
