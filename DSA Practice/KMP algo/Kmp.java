563b580e79675f23d87b9aa4440ee1c3b918cc98
sumamakhan761 <164807432+sumamakhan761@users.noreply.github.com>
2025-03-01 15:11:45 +0530

    Update Kmp.java

--- DSA Practice/KMP algo/Kmp.java
+++ DSA Practice/KMP algo/Kmp.java
@@ -1,1 +1,5 @@
 https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+https://leetcode.com/problems/longest-happy-prefix/description/
+
+https://leetcode.com/problems/repeated-string-match/
