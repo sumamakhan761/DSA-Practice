638efc05e88bebe445e0add9df92b231213a96d0
sumamakhan761 <164807432+sumamakhan761@users.noreply.github.com>
2025-03-05 16:30:43 +0530

    Update Binary_Tree&Binary_Search_Tree.java

--- DSA Practice/Binary_Tree&Binary_Search_Tree/Binary_Tree&Binary_Search_Tree.java
+++ DSA Practice/Binary_Tree&Binary_Search_Tree/Binary_Tree&Binary_Search_Tree.java
@@ -1,3 +1,5 @@
+Easy
+
 https://leetcode.com/problems/binary-tree-inorder-traversal/description/?envType=problem-list-v2&envId=binary-tree
 
 https://leetcode.com/problems/binary-tree-preorder-traversal/description/?envType=problem-list-v2&envId=binary-tree
@@ -35,4 +37,47 @@ https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/?en
 https://leetcode.com/problems/range-sum-of-bst/description/?envType=problem-list-v2&envId=binary-tree
 
 https://leetcode.com/problems/univalued-binary-tree/description/?envType=problem-list-v2&envId=binary-tree
+
+
+medium
+
+https://leetcode.com/problems/binary-tree-level-order-traversal/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/unique-binary-search-trees/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/validate-binary-search-tree/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/recover-binary-search-tree/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/maximum-width-of-binary-tree/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/delete-node-in-a-bst/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/binary-search-tree-iterator/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/description/
+
+https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree/
+
+https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/binary-tree-right-side-view/description/?envType=problem-list-v2&envId=binary-tree
+
+https://leetcode.com/problems/find-bottom-left-tree-value/description/?envType=problem-list-v2&envId=binary-tree
+
+
 
