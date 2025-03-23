02bbecb2615e8d4f16f005f1bc035a84f478b7fb
sumamakhan761 <164807432+sumamakhan761@users.noreply.github.com>
2025-03-23 09:54:48 +0530

    Update Graph.java

--- DSA Practice/Graph/Graph.java
+++ DSA Practice/Graph/Graph.java
@@ -1,1 +1,5 @@
+https://leetcode.com/problems/find-the-town-judge/description/?envType=problem-list-v2&envId=graph
 
+https://leetcode.com/problems/find-if-path-exists-in-graph/description/?envType=problem-list-v2&envId=graph
+
+https://leetcode.com/problems/find-center-of-star-graph/description/?envType=problem-list-v2&envId=graph
